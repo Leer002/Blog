49bd1b1574b70ebe2d142515a60713f88bae4a68
Leer002 <164624183+Leer002@users.noreply.github.com>
2024-11-19 16:31:39 +0330

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.views import View
 from django.shortcuts import render, redirect
+from django.views import View
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
